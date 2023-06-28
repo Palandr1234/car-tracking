4bc8efe3d3d47e5805a44f10166bc84c6b0dcd34
Andrey Palaev <a.palaev@innopolis.university>
2023-06-28 17:06:41 +0000

    = update __init__.py

--- car_tracking/models/object_tracking/__init__.py
+++ car_tracking/models/object_tracking/__init__.py
@@ -1,2 +1,2 @@
-from car_tracking.models.object_tracking.base import BaseTracker
+from car_tracking.models.object_tracking.base import BaseTracker, TrackerPredictionEntry
 from car_tracking.models.object_tracking.sort import SORTTracker
