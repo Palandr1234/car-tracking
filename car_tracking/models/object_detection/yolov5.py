4bf001a8993239d89b12a6d64ccefd444d0a5d34
Andrey Palaev <a.palaev@innopolis.university>
2023-06-28 20:06:19 +0000

    + add possibility to specify target classes

--- car_tracking/models/object_detection/yolov5.py
+++ car_tracking/models/object_detection/yolov5.py
@@ -1,4 +1,4 @@
-from typing import Union
+from typing import Union, Optional
 
 import torch
 import numpy as np
@@ -9,15 +9,20 @@ from car_tracking.models.object_detection.base import BaseObjectDetector, Object
 class YOLOv5ObjectDetector(BaseObjectDetector):
     """Class for YOLOv5 object detection model"""
 
-    def __init__(self, weights_path: str, device: Union[str, torch.device]) -> None:
+    def __init__(self, weights_path: str, device: Union[str, torch.device], conf_thresh: float,
+                 target_classes: Optional[list[int]] = None) -> None:
         """
         Initialize YOLOv5 object detector
 
         Args:
             weights_path: path to the YOLOv5 weights
             device: computing device - either str or torch.device
+            conf_thresh: confidence threshold
+            target_classes: target classes of the model; if None, all classes are considered
         """
         self.model = torch.hub.load('ultralytics/yolov5', 'custom', path=weights_path, device=device, _verbose=False)
+        self.model.conf = conf_thresh
+        self.target_classes = target_classes
 
     def __call__(self, img: np.ndarray[np.uint8]) -> list[ObjectDetectorPredictionEntry]:
         """
@@ -33,5 +38,6 @@ class YOLOv5ObjectDetector(BaseObjectDetector):
         result = []
         for det in detections:
             det = det.cpu()
-            result.append(ObjectDetectorPredictionEntry(BoundingBox(det[:4], 'xyxy', det[5], det[4])))
+            if self.target_classes is None or det[-1] in self.target_classes:
+                result.append(ObjectDetectorPredictionEntry(BoundingBox(det[:4], 'xyxy', det[5], det[4])))
         return result
