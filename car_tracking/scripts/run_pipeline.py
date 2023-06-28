a481ffe3b422023f4388b56b58866d6c46087f3c
Andrey Palaev <a.palaev@innopolis.university>
2023-06-28 18:36:58 +0000

    + script for running pipeline

---
+++ car_tracking/scripts/run_pipeline.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+
+import hydra
+from omegaconf import DictConfig
+
+from car_tracking.pipelines.base import BasePipeline
+
+
+@hydra.main(version_base=None, config_path='../configs', config_name='tracking.yaml')
+def main(config: DictConfig) -> None:
+    save_dir = Path(config.data.save_dir)
+    save_dir.mkdir(exist_ok=True, parents=True)
+    pipeline: BasePipeline = hydra.utils.instantiate(config.pipeline)
+    pipeline.run(Path(config.data.path), save_dir)
+
+
+if __name__ == '__main__':
+    main()
